c6c221a47cff64010632b5b83c9a2c401856a27a
LinaLinux <damaris.bohorquez@gmail.com>
2022-11-13 17:37:24 -0800

    Added another javascript button

--- ipsum.js
+++ ipsum.js
@@ -1,3 +1,7 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
+
+function aggressive() {
+    document.getElementById("angry").innerHTML = "Thank goodness...";
+  }
