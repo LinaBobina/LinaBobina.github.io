d147657497c24ada8bc3228ed0e6bdae7499c7cd
linaChromeOS <lina.bohorquez@shopify.com>
2023-03-04 00:00:00 -0700

    replace console.logs with innerHTML. Adjusted button behaviour to allow playerDialog text to fade in

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -15,21 +15,28 @@ let computerChoice = ["rock", "paper", "scissors"];
 //////////// Computer's random generated determined choice // 
 function getComputerChoice(){
     let random = computerChoice[Math.floor(Math.random()*computerChoice.length)];
-    console.log("Computer picks " + random + "!")
+    document.getElementById("computerDialog").innerHTML = "Computer picked " + random + "!"
     return random
 }
 
 
 // Player button and choice being stored
 const buttons = document.querySelectorAll('button');
+const playerDialog = document.getElementById("playerDialog");
 
 Array.from(buttons).forEach(button => { 
     button.addEventListener('click', () => {
         let playerChoice = getPlayerChoice();
         let computerChoice = getComputerChoice();
-        console.log(singleRound(playerChoice, computerChoice))
+        playerDialog.innerHTML = `Player picked ${playerChoice}!`;
+        playerDialog.classList.remove('fade-in');
+        void playerDialog.offsetWidth;
+        playerDialog.classList.add('fade-in');
+        return singleRound(playerChoice, computerChoice)
     });
 });
+
+
 
 
 function getPlayerChoice () {
@@ -37,13 +44,10 @@ function getPlayerChoice () {
 
     switch (choice) {
         case 'rockBtn':
-            console.log("Player picks Rock!")
             return "rock";
         case 'paperBtn':
-            console.log("Player picks Paper!")
             return "paper";
         case 'scissorsBtn':
-            console.log("Player picks Scissors!")
             return "scissors";
         default:
             console.log("nothing");
@@ -57,30 +61,39 @@ function singleRound(playerChoice, computerChoice){
     let tie = "No one won! Try again."
 
     if (playerChoice === "scissors" && computerChoice === "paper"){
+        document.getElementById("resultsDialog").innerHTML = winner
         return winner
     }
     else if (playerChoice === "scissors" && computerChoice === "rock"){
+        document.getElementById("resultsDialog").innerHTML = loser
         return loser
     }
     else if (playerChoice === "scissors" && computerChoice === "scissors"){
+        document.getElementById("resultsDialog").innerHTML = tie
         return tie
     }
     else if (playerChoice === "rock" && computerChoice === "paper"){
+        document.getElementById("resultsDialog").innerHTML = loser
         return loser
     }
     else if (playerChoice === "rock" && computerChoice === "scissors"){
+        document.getElementById("resultsDialog").innerHTML = winner
         return winner
     }
     else if (playerChoice === "rock" && computerChoice === "rock"){
+        document.getElementById("resultsDialog").innerHTML = tie
         return tie
     }
     else if (playerChoice === "paper" && computerChoice === "scissors"){
+        document.getElementById("resultsDialog").innerHTML = loser
         return loser
     }
     else if (playerChoice === "paper" && computerChoice === "rock"){
+        document.getElementById("resultsDialog").innerHTML = winner
         return winner
     }
     else if (playerChoice === "paper" && computerChoice === "paper"){
+        document.getElementById("resultsDialog").innerHTML = tie
         return tie
     }
 
