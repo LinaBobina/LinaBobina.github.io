6ac16fa00566516b904f28fa1839d8d631608e49
linaChromeOS <lina.bohorquez@shopify.com>
2023-03-19 10:57:01 -0600

    addEventListener added to pixel size button. The content is now determined by the pixel size multiplied by itself

--- calculator.js
+++ calculator.js
@@ -1,10 +1,33 @@
 const container = document.querySelector("#container");
+const content = document.createElement("div");
+container.appendChild(content);
 
-for (let i = 0; i <=255; i++) {
-    const content = document.createElement("div");
-    content.classList.add("content");
-    container.appendChild(content);
-}
+
+// Grabbing pixel dimension from HTML button
+function pixelBtnSelected() {
+    const pixelBtn = document.querySelector('#pixelBtn');
+    return parseInt(pixelBtn.dataset.pixelsize);
+  };
+
+// 
+const pixelBtn = document.querySelector('#pixelBtn');
+pixelBtn.addEventListener("click", () => {
+    const pixelSize = pixelBtnSelected();
+    for (let i = 0; i < (pixelSize * pixelSize); i++) {
+        const content = document.createElement("div");
+        content.classList.add("content");
+        container.appendChild(content);
+
+        const contentHover = document.querySelectorAll(".content");
+        contentHover.forEach(hover => {
+        hover.addEventListener("mouseover", () => {
+        console.log("hover event");
+        event.target.style.backgroundColor = "black";
+    });
+});
+
+    }
+})
 
 const contentHover = document.querySelectorAll(".content");
 contentHover.forEach(hover => {
