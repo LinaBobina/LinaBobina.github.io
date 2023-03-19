923f3d1e13744716364b3c8ad036c45d3664e75e
linaChromeOS <lina.bohorquez@shopify.com>
2023-03-19 08:24:36 -0600

    add hover event on divs to change it to black

--- calculator.js
+++ calculator.js
@@ -1,9 +1,15 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i <=15; i++) {
+for (let i = 0; i <=255; i++) {
     const content = document.createElement("div");
     content.classList.add("content");
-    content.textContent = "0";
     container.appendChild(content);
 }
 
+const contentHover = document.querySelectorAll(".content");
+contentHover.forEach(hover => {
+    hover.addEventListener("mouseover", () => {
+        console.log("hover event");
+        event.target.style.backgroundColor = "black";
+    });
+});
